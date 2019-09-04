2d364e5bdb3b96e347d48b81cd6b1d9f64b6bd99
Joosep Pata <joosep.pata@cern.ch>
2019-09-04 20:15:22 +0200

    up

--- fwlite/pflow.py
+++ fwlite/pflow.py
@@ -1,5 +1,6 @@
 import ROOT
 import sys
+import numpy as np
 from DataFormats.FWLite import Events, Handle
 
 filename = sys.argv[1]
@@ -35,9 +36,18 @@ class EventDesc:
 
 class Output:
     def __init__(self):
-        pass
+        self.tfile = ROOT.TFile("out.root", "RECREATE")
+        self.clusters = ROOT.TTree("clusters", "clusters")
+        self.clusters_layer = np.zeros(1, dtype=np.int32)
+        self.clusters_x = np.zeros(1, dtype=np.float32)
+        self.clusters.Branch("layer", self.clusters_layer, "layer/I")
+
+    def close(self):
+        self.tfile.Write()
+        self.tfile.Close()
  
 evdesc = EventDesc()
+output = Output()
 
 # loop over events
 for event in events:
@@ -46,27 +56,29 @@ for event in events:
 
     evdesc.get(event)
 
-    genpart = evdesc.genparticle.product()
-    for gp in genpart:
-        if gp.status() == 2:
-            print("genpart", eventId[2], gp.energy(), gp.px(), gp.py(), gp.pz(), gp.pdgId())
+    #genpart = evdesc.genparticle.product()
+    #for gp in genpart:
+    #    if gp.status() == 2:
+    #        print("genpart", eventId[2], gp.energy(), gp.px(), gp.py(), gp.pz(), gp.pdgId())
     
     pfcluster_ecal = evdesc.pfcluster_ecal.product()
     for cl in pfcluster_ecal:
-        print("ecal", eventId[2], cl.depth(), int(cl.layer()), cl.energy(), cl.x(), cl.y(), cl.z())
+        output.clusters_layer[0] = cl.layer()
+        output.clusters_x[0] = cl.x()
+        output.clusters.Fill()
     
-    pfcluster_hcal = evdesc.pfcluster_hcal.product()
-    for cl in pfcluster_hcal:
-        print("hcal", eventId[2], cl.depth(), int(cl.layer()), cl.energy(), cl.x(), cl.y(), cl.z())
-    
-    pfcluster_hf = evdesc.pfcluster_hf.product()
-    for cl in pfcluster_hf:
-        print("hf", eventId[2], cl.depth(), int(cl.layer()), cl.energy(), cl.x(), cl.y(), cl.z())
-    
-    tracks = evdesc.tracks.product()
-    for c in tracks:
-        print("trk", eventId[2], c.charge(), c.innerMomentum().x(), c.innerMomentum().y(), c.innerMomentum().z(), c.px(), c.py(), c.pz(), c.outerMomentum().x(), c.outerMomentum().y(), c.outerMomentum().z())
-    
-    pfcand = evdesc.pfcand.product()
-    for c in pfcand:
-        print("cand", eventId[2], c.energy(), c.px(), c.py(), c.pz(), c.pdgId())
+    #pfcluster_hcal = evdesc.pfcluster_hcal.product()
+    #for cl in pfcluster_hcal:
+    #    print("hcal", eventId[2], cl.depth(), int(cl.layer()), cl.energy(), cl.x(), cl.y(), cl.z())
+    #
+    #pfcluster_hf = evdesc.pfcluster_hf.product()
+    #for cl in pfcluster_hf:
+    #    print("hf", eventId[2], cl.depth(), int(cl.layer()), cl.energy(), cl.x(), cl.y(), cl.z())
+    #
+    #tracks = evdesc.tracks.product()
+    #for c in tracks:
+    #    print("trk", eventId[2], c.charge(), c.innerMomentum().x(), c.innerMomentum().y(), c.innerMomentum().z(), c.px(), c.py(), c.pz(), c.outerMomentum().x(), c.outerMomentum().y(), c.outerMomentum().z())
+    #
+    #pfcand = evdesc.pfcand.product()
+    #for c in pfcand:
+    #    print("cand", eventId[2], c.energy(), c.px(), c.py(), c.pz(), c.pdgId())
